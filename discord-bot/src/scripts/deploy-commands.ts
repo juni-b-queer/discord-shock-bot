2f5a82a82ceb0033f5c538de92d0e32d55d0bbc7
Juni <june@juniper-bender.tech>
2025-03-27 21:57:38 -0500

    ts ignore

--- discord-bot/src/scripts/deploy-commands.ts
+++ discord-bot/src/scripts/deploy-commands.ts
@@ -1,49 +1,49 @@
-import { REST, Routes } from 'discord.js';
-import dotenv from 'dotenv';
-import fs from 'node:fs';
-import path from 'node:path';
-import { db } from '../db';
-import { guildsTable } from '../db/schema';
+import { REST, Routes } from 'discord.js'
+import dotenv from 'dotenv'
+import fs from 'node:fs'
+import path from 'node:path'
+import { db } from '../db'
+import { guildsTable } from '../db/schema'
 
-dotenv.config({ path: path.join(__dirname, '../../../.env') });
+dotenv.config({ path: path.join(__dirname, '../../../.env') })
 
-const commands = [];
+const commands = []
 // Grab all the command folders from the commands directory you created earlier
-const foldersPath = path.join(__dirname, '../commands');
+const foldersPath = path.join(__dirname, '../commands')
 
-const commandFolders = fs.readdirSync(foldersPath);
+const commandFolders = fs.readdirSync(foldersPath)
 
 for (const folder of commandFolders) {
     // Grab all the command files from the commands directory you created earlier
-    const commandsPath = path.join(foldersPath, folder);
+    const commandsPath = path.join(foldersPath, folder)
     const commandFiles = fs
         .readdirSync(commandsPath)
-        .filter((file) => file.endsWith('.ts'));
+        .filter((file) => file.endsWith('.ts'))
     // Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
     for (const file of commandFiles) {
-        const filePath = path.join(commandsPath, file);
-        const command = require(filePath);
+        const filePath = path.join(commandsPath, file)
+        const command = require(filePath)
         if ('data' in command && 'execute' in command) {
-            commands.push(command.data.toJSON());
+            commands.push(command.data.toJSON())
         } else {
             console.log(
                 `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
-            );
+            )
         }
     }
 }
 
 // Construct and prepare an instance of the REST module
-const rest = new REST().setToken(process.env.DISCORD_TOKEN ?? '');
+const rest = new REST().setToken(process.env.DISCORD_TOKEN ?? '')
 
 // and deploy your commands!
-(async () => {
+;(async () => {
     try {
-        const guilds = await db.select().from(guildsTable);
+        const guilds = await db.select().from(guildsTable)
         for (const guild of guilds) {
             console.log(
                 `Started refreshing ${commands.length} application (/) commands for ${guild.guildId}.`
-            );
+            )
 
             // The put method is used to fully refresh all commands in the guild with the current set
             const data = await rest.put(
@@ -52,17 +52,17 @@ const rest = new REST().setToken(process.env.DISCORD_TOKEN ?? '');
                     guild.guildId
                 ),
                 { body: commands }
-            );
+            )
 
-            // @ts-ignore
             console.log(
+                // @ts-ignore
                 `Successfully reloaded ${data.length} application (/) commands for ${guild.guildId}.`
-            );
+            )
         }
-        console.log('Done');
-        return process.exit(0);
+        console.log('Done')
+        return process.exit(0)
     } catch (error) {
         // And of course, make sure you catch and log any errors!
-        console.error(error);
+        console.error(error)
     }
-})();
+})()
